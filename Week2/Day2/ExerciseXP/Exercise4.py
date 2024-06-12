ef2aedbcf0de2e68ef6ca1fd3645af5ffadab1b5
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-06-12 21:00:02 +0300

    added week 2 day 2 exercise 4

--- Week2/Day2/ExerciseXP/Exercise4.py
+++ Week2/Day2/ExerciseXP/Exercise4.py
@@ -4,13 +4,25 @@ Create a list containing the following sequence 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5 (
 Use another way to generate a sequence of floats
 """
 
-
+# Define sequence by it's parameters
 start_value = 1.5
 end_value = 5
 step_value = 0.5
-list_range = int(((end_value - start_value) * step_value) + 1)
+list_range = int(((end_value - start_value) / step_value) + 1)
 
-# creating a list using list comprehension
-floats_list = [int(start_value + i * step_value) if (start_value + i * step_value).is_integer() else (start_value + i * step_value) for i in range(list_range)]
+# Generate the sequence of floats using list comprehension
+floats_list = [start_value + i * step_value for i in range(list_range)]
 
-print("The resulting list:",floats_list)
+# Convert elements to integers if they have no decimal part
+result_list = [int(num) if num % 1 == 0 else num for num in floats_list]
+
+print("The resulting list:", result_list)
+
+"""
+Other ways to generate sequence without hardcoding numbers
+will require using numpy or itertools libraries, 
+or I don't understand the task proprly.
+ 
+Since we didn't cover the topic of libraries, 
+I assume I misunderstand something here.
+"""
