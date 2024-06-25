4e957a8523d22ec9ffb53f8b01c5729b8574669c
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-06-25 13:02:03 +0300

    added week 3 day 3 exercise 2 folder with related files

---
+++ Week3/Day3/ExerciseXP/Exercise2/exercise_one.py
@@ -0,0 +1,2 @@
+import func as my_functions
+my_functions.sum_two_numbers(0.5,999)
---
+++ Week3/Day3/ExerciseXP/Exercise2/func.py
@@ -0,0 +1,9 @@
+def sum_two_numbers(num1 = int | float, num2 = int | float):
+  """Sums two numbers and prints the result.
+
+  Args:
+      num1: The first number to be summed.
+      num2: The second number to be summed.
+  """
+  total = num1 + num2
+  print(f"The sum of {num1} and {num2} is: {total}")
