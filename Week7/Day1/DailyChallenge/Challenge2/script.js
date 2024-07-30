b62359ba6e78ca8e4dba41936ae79763efe34575
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-07-30 03:44:00 +0300

    added correction to Week 7 day 1 daily challenge 2

--- Week7/Day1/DailyChallenge/Challenge2/script.js
+++ Week7/Day1/DailyChallenge/Challenge2/script.js
@@ -52,6 +52,7 @@ const morse = `{
 // this function converts the morse json string provided above to a morse javascript object.
 // if the morse javascript object is empty, throw an error(use reject)
 // else return the morse javascript object(use resolve)
+
 function toJs(morseString) {
     return new Promise((resolve, reject) => {
         try {
@@ -74,12 +75,13 @@ function toJs(morseString) {
 // else return an array with the morse translation of the user’s word.
 // if the user enters the word "Hello", the promise resolves with this value["....", ".", ".-..", ".-..", "---"]
 // if the user entered the word "¡Hola!", the promise rejects because the character "¡" doesn't exist in the morse javascript object
+
 function toMorse(morseObject) {
     return new Promise((resolve, reject) => {
-        const word = prompt("Enter a word or sentence:");
+        const userInput = prompt("Enter a word or sentence:");
         const morseCode = [];
 
-        for (const char of word) {
+        for (const char of userInput) {
             const lowerCaseChar = char.toLowerCase();
             if (morseObject[lowerCaseChar]) {
                 morseCode.push(morseObject[lowerCaseChar]);
@@ -91,18 +93,19 @@ function toMorse(morseObject) {
             }
         }
 
-        resolve(morseCode);
+        resolve({ userInput, morseCode }); // requires userInput to build the proper output paragraph
     });
 }
 
 // The third function called joinWords(morseTranslation), takes one argument: the morse translation array
 
 // this function joins the morse translation by using line break and display it on the page(ie.On the DOM)
+
 function joinWords(morseTranslation) {
     return new Promise((resolve) => {
-        const joinedMorse = morseTranslation.join(' ');
+        const joinedMorse = morseTranslation.morseCode.join('<br>');
         const outputElement = document.getElementById('output');
-        outputElement.textContent = `${joinedMorse}`;
+        outputElement.innerHTML = `You entered: ${morseTranslation.userInput}<br>Morse code:<br>${joinedMorse}`;
         resolve();
     });
 }
