4394b7aca331e46c0a24fba291be13e9c5090373
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-08-08 19:36:01 +0300

    added all files in order to clean the list of changes

---
+++ Week1/challenge1_sorting.py
@@ -0,0 +1,13 @@
+def sort_words(words_string):
+  """Sorts words in a comma-separated string alphabetically and returns a sorted string."""
+  words_list = words_string.split(",") # Create a list from a comma-separated string
+  sorted_words = sorted(word for word in words_list)  # List comprehension for sorting a list of words
+  sorted_string = ",".join(sorted_words) # Create a comma-separated string of words from a list
+  return sorted_string
+
+# Get user input
+user_input = input("Enter comma-separated words: ")
+
+# Sort and format user input
+sorted_output = sort_words(user_input)
+print(sorted_output)
