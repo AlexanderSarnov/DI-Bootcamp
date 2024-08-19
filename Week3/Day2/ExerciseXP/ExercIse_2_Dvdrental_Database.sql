13d63cb026d6db803f3d158363fede01c06a6ffc
Alexander Sarnov <alexander.sarnov@gmail.com>
2024-08-20 00:10:24 +0300

    added week 3 day 2 exercisesXP

---
+++ Week3/Day2/ExerciseXP/ExercIse_2_Dvdrental_Database.sql
@@ -0,0 +1,152 @@
+-- -- In the dvdrental database write a query to select all the columns from the “customer” table.
+-- SELECT * FROM customer;
+
+-- -- Write a query to display the names (first_name, last_name) using an alias named “full_name”.
+-- SELECT (first_name, last_name) AS full_name FROM customer;
+
+-- -- Lets get all the dates that accounts were created. Write a query to select all the create_date from the “customer” table (there should be no duplicates).
+-- SELECT DISTINCT create_date FROM customer;
+
+-- -- Write a query to get all the customer details from the customer table, it should be displayed in descending order by their first name.
+-- SELECT * FROM customer ORDER BY first_name DESC;
+
+-- -- Write a query to get the film ID, title, description, year of release and rental rate in ascending order according to their rental rate.
+-- SELECT 
+-- 	film_id,
+-- 	title,
+-- 	description,
+-- 	release_year,
+-- 	rental_rate
+-- FROM 
+-- 	film 
+-- ORDER BY rental_rate ASC;
+
+-- -- Write a query to get the address, and the phone number of all customers living in the Texas district, these details can be found in the “address” table.
+-- SELECT 
+-- 	address,
+-- 	phone
+-- FROM 
+-- 	address
+-- WHERE 
+-- 	district='Texas' OR district='texas';
+
+-- -- Write a query to retrieve all movie details where the movie id is either 15 or 150.
+-- SELECT * FROM film
+-- 	WHERE film_id = 15 OR film_id = 150;
+
+-- -- Write a query which should check if your favorite movie exists in the database. Have your query get the film ID, title, description, length and the rental rate, these details can be found in the “film” table.
+-- SELECT
+-- 	film_id,
+-- 	title,
+-- 	description,
+-- 	length,
+-- 	rental_rate
+-- FROM
+-- 	film
+-- WHERE 
+-- 	title='My Favorite Movie';
+
+-- -- No luck finding your movie? Maybe you made a mistake spelling the name. Write a query to get the film ID, title, description, length and the rental rate of all the movies starting with the two first letters of your favorite movie.
+-- SELECT 
+-- 	film_id, 
+-- 	title, 
+-- 	description, 
+-- 	length, 
+-- 	rental_rate
+-- FROM 
+-- 	film
+-- WHERE 
+-- 	title LIKE 'My%';
+
+-- -- Write a query which will find the 10 cheapest movies.
+-- WITH MovieRanks AS (
+--   SELECT
+--     film_id,
+--     title,
+--     rental_rate,
+--     ROW_NUMBER() 
+-- 	OVER (ORDER BY rental_rate ASC) AS row_num
+--   FROM film
+-- )
+-- SELECT 
+-- 	film_id, 
+-- 	title, 
+-- 	rental_rate
+-- FROM 
+-- 	MovieRanks
+-- WHERE row_num BETWEEN 1 AND 10;
+
+-- -- Not satisfied with the results. Write a query which will find the next 10 cheapest movies.
+-- -- Bonus: Try to not use LIMIT.
+-- WITH MovieRanks AS (
+--   SELECT
+--     film_id,
+--     title,
+--     rental_rate,
+--     ROW_NUMBER() 
+-- 	OVER (ORDER BY rental_rate ASC) AS row_num
+--   FROM film
+-- )
+-- SELECT 
+-- 	film_id, 
+-- 	title, 
+-- 	rental_rate
+-- FROM 
+-- 	MovieRanks
+-- WHERE row_num BETWEEN 11 AND 20;
+
+-- -- Write a query which will join the data in the customer table and the payment table. You want to get the first name and last name from the curstomer table, as well as the amount and the date of every payment made by a customer, ordered by their id (from 1 to…).
+-- SELECT 
+-- 	c.customer_id, 
+-- 	c.first_name, 
+-- 	c.last_name, 
+-- 	p.amount, 
+-- 	p.payment_date
+-- FROM customer c
+-- INNER JOIN payment p ON c.customer_id = p.customer_id
+-- ORDER BY c.customer_id; 
+
+-- -- You need to check your inventory. Write a query to get all the movies which are not in inventory.
+-- SELECT 
+-- 	f.film_id, 
+-- 	f.title
+-- FROM film f
+-- LEFT JOIN inventory i ON f.film_id = i.film_id
+-- WHERE i.film_id IS NULL;
+
+-- -- Write a query to find which city is in which country.
+-- SELECT 
+-- 	ci.city, 
+-- 	co.country
+-- FROM city ci
+-- INNER JOIN country co ON ci.country_id = co.country_id;
+
+-- Bonus You want to be able to see how your sellers have been doing? Write a query to get the customer’s id, names (first and last), the amount and the date of payment ordered by the id of the staff member who sold them the dvd.
+-- SELECT 
+-- 	c.customer_id, 
+-- 	c.first_name, 
+-- 	c.last_name, 
+-- 	p.amount, 
+-- 	p.payment_date, 
+-- 	s.staff_id
+-- FROM customer c
+-- INNER JOIN payment p ON c.customer_id = p.customer_id
+-- INNER JOIN staff s ON p.staff_id = s.staff_id
+-- ORDER BY s.staff_id;
+
+-- -- Additional sales analysis by customer and by stuff (depending on how to comment different query lines, may sometimes return error even when all is OK, need to run again)
+-- SELECT 
+-- 	c.customer_id, 
+-- 	c.first_name, 
+-- 	c.last_name, 
+-- 	SUM(p.amount) AS total_amount,
+-- 	s.staff_id
+-- FROM customer c
+-- INNER JOIN payment p ON c.customer_id = p.customer_id
+-- INNER JOIN staff s ON p.staff_id = s.staff_id
+-- GROUP BY 
+-- 	c.customer_id, 
+-- 	c.first_name, 
+-- 	c.last_name, 
+-- 	s.staff_id
+-- ORDER BY s.staff_id;
---
+++ Week3/Day2/ExerciseXP/Exercise_1_Items_and_customers.sql
@@ -0,0 +1,37 @@
+-- -- All items, ordered by price (lowest to highest).
+-- SELECT 
+-- 	item_name, 
+-- 	item_price 
+-- FROM 
+-- 	items 
+-- ORDER BY
+-- 	item_price ASC;
+
+-- -- Items with a price above 80 (80 included), ordered by price (highest to lowest).
+-- SELECT
+-- 	item_name,
+-- 	item_price
+-- FROM
+-- 	items
+-- WHERE
+-- 	item_price >= 80 
+-- ORDER BY
+-- 	item_price DESC
+	
+-- -- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+-- SELECT 
+-- 	customer_name, 
+-- 	customer_last_name 
+-- FROM 
+-- 	customers
+-- ORDER BY
+-- 	customer_name ASC
+-- LIMIT 3;
+
+-- -- All last names (no other columns!), in reverse alphabetical order (Z-A)
+-- SELECT
+-- 	customer_last_name
+-- FROM
+-- 	customers
+-- ORDER BY
+-- 	customer_last_name DESC
